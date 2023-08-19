4bab8a52300ef9a4f5bb2d1825c2de7c89248985
hyunji3183 <133857456+hyunji3183@users.noreply.github.com>
2023-08-19 14:36:48 +0900

    0819

--- sejong/README.md
+++
@@ -1,6 +0,0 @@
-cd cd# Sejong Center for the Performing Arts
-
-Sejong Center for the Performing Arts team project
-
-
-이현
--- sejong/js/location.js
+++
@@ -1,5 +0,0 @@
-$('body').prepend('<header></header>');
-$('header').load('../html/header.html')
-
-$('body').append('<footer></footer>');
-$('footer').load('../html/sub_foot_aside.html')
