5a65443bd42d0c4123a140c83dc203bc7f7cbe11
hyunji3183 <chj3183@gmail.com>
2023-08-21 18:20:40 +0900

    0821

---
+++ sejong backup/js/locotion_copy.js
@@ -0,0 +1,10 @@
+new daum.roughmap.Lander({
+    "timestamp": "1692608974725",
+    "key": "2fwiv",
+}).render();
+
+const $btn_kakao = document.querySelector('.kakao_map');
+$btn_kakao.onclick = function(){
+    document.querySelector('.guide_img').innerHTML = '';
+    
+}
