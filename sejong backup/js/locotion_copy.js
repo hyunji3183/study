497849f28e343417c22e3e52aa36c78bbf9da6fc
hyunji3183 <chj3183@gmail.com>
2023-08-22 18:16:23 +0900

    0822

--- sejong backup/js/locotion_copy.js
+++ sejong backup/js/locotion_copy.js
@@ -1,26 +1,38 @@
-new daum.roughmap.Lander({
-    "timestamp": "1692686139957",
-    "key": "2fxzw",
-    "mapWidth": "900",
-    "mapHeight": "600"
-}).render();
-
 const $btn_naver = document.querySelector('.naver_map');
 const $btn_kakao = document.querySelector('.kakao_map');
 $btn_kakao.onclick = function () {
-    const map_img = document.querySelector('.guide_img .img img');
-    const kakaomap = document.querySelector('.guide_img .map div');
+    const map_img = document.querySelector('.guide_img');
     map_img.classList.add('on')
-    kakaomap.classList.add('on')
 }
 
-$btn_kakao.forEach(
-        this.classList.add('on')
-);
+//버튼 마우스오버
+const changenaver = document.querySelector('.naver_map p img');
+function mouseover1 (){
+    changenaver.setAttribute("src","../imgs/location/location_naver_color_(2).png");
+}
+function mouseleave1 (){
+    changenaver.setAttribute("src","../imgs/location/location_naver_color.png");
+}
+
+const changekakao = document.querySelector('.kakao_map p img');
+function mouseover2 (){
+    changekakao.setAttribute("src","../imgs/location/location_map_color_(2).png");
+}
+function mouseleave2 (){
+    changekakao.setAttribute("src","../imgs/location/location_map_color.png");
+}
 
 
+new daum.roughmap.Lander({
+    "timestamp" : "1692694545504",
+    "key" : "2fx6r",
+    "mapWidth" : "900",
+    "mapHeight" : "600",
+}).render();
 
-
-
-
-
+new daum.roughmap.Lander({
+    "timestamp" : "1692695583825",
+    "key" : "2fx74",
+    "mapWidth" : "800",
+    "mapHeight" : "400"
+}).render();
